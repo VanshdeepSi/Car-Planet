ea524319080ff7877ebf78c5a0df60f65ef43369
VanshdeepSi <vanshdeepsb@gmail.com>
2026-07-03 02:21:51 +0530

    Fix typescript error in mock cars

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,8 @@ const MOCK_CARS: Car[] = [
       "https://lh3.googleusercontent.com/aida/AP1WRLu0wIvbxffZmMFjhDXLln-GbyV1SJlERJ2hzmomqQORUC8Hc4Ow7woKYs0fhl2agZYSrvAFF-TUPcOiu--JCE-8SpokKD8bjAe3ajeF9ITREFG0Luii4cV0Xif4Osz8d2rKM8R2mOiqthVpC71U-xU--On1Z1matHkTkAoskPV-_YzwDdVTsQMxp4PjzNsB6CJnlfZny5f8cB3mNI4XAV0-GM2uYKWVOnFWivYG2a_B1EMLQqd0PjdsRw"
     ],
     description: null,
+    category: "SUV",
+    is_exclusive: false,
     created_at: new Date().toISOString(),
   },
   {
@@ -37,6 +39,8 @@ const MOCK_CARS: Car[] = [
       "https://lh3.googleusercontent.com/aida/AP1WRLuVwYJcSRPDNLQznqUj6pNu3LQQKVnikqlXZNZv7viRDM-lo5eUs_VtI2XctlXojWgix61u9KXwHGugCXo1bmBgtNRww21hYjIRu_tAJZ4i38L8jg_ZdIqISf0QgwHnOzdvI-UXX-SEZ1J2NLvN5RgSxOzWOZjmPwby9GIl-pWGNRFIt-QQ0uTcDPFt6s7Hi6pz1Qws3yjtQSxojBAOE-RXSAy1sXGyB0Y2NlLvqQcX3qI5vXKkITJzJlc"
     ],
     description: null,
+    category: "SUV",
+    is_exclusive: false,
     created_at: new Date().toISOString(),
   },
   {
@@ -55,6 +59,8 @@ const MOCK_CARS: Car[] = [
       "https://lh3.googleusercontent.com/aida/AP1WRLvJYyiXPKWZblUi9vxuaV_nafYT8SCwjELcxdm4T6M6UZNtvwZ9wRV3dbdsMRMAQKRyA5djsndZ_vMx_BUl1BSnBQAua6ZsF5sbU0sYkhwNal_WPYK2szXA6Rl9UOzG6gAEN_isPA87dlr-2kgk6u5kx9BpTcw0PRZKbwAQLWxw1ldDJsKEcp3gxQ7F-6GlY3RIBYPi2c8kUCCL3ZA3yLcTq8g58fXE3KgixXOoxfIhWOSirRTel-60yPg"
     ],
     description: null,
+    category: "SUV",
+    is_exclusive: true,
     created_at: new Date().toISOString(),
   }
 ];
